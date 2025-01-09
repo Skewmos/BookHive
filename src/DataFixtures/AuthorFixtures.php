1fe03e31bf7352ec61c64dff83c0ff7a0bd6a8fe
Skewmos <jeremielegrand021@gmail.com>
2025-01-09 15:32:12 +0100

    :construction: Creation of fixtures for Authors

---
+++ src/DataFixtures/AuthorFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\AuthorFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AuthorFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+       AuthorFactory::new()->createMany(5);
+
+        $manager->flush();
+    }
+}
