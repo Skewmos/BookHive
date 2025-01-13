d7b9bc75015bb13f3aa68131030cb1246f9d2996
Skewmos <jeremielegrand021@gmail.com>
2025-01-13 22:34:26 +0100

    :construction: creating the migration

---
+++ migrations/Version20250113213144.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250113213144 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('ALTER TABLE author ALTER last_name TYPE VARCHAR(50)');
+        $this->addSql('ALTER TABLE author ALTER first_name TYPE VARCHAR(50)');
+        $this->addSql('ALTER TABLE author ALTER pseudonym TYPE VARCHAR(40)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('ALTER TABLE author ALTER last_name TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE author ALTER first_name TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE author ALTER pseudonym TYPE VARCHAR(255)');
+    }
+}
