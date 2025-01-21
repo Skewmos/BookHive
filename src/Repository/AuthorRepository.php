d8e0b58afc99779c84ec27f92a393bf1c8412d63
Skewmos <jeremielegrand021@gmail.com>
2025-01-21 18:17:29 +0100

    :fire:removing old packages

--- src/Repository/AuthorRepository.php
+++ src/Repository/AuthorRepository.php
@@ -5,31 +5,15 @@ namespace App\Repository;
 use App\Entity\Author;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Knp\Component\Pager\Pagination\PaginationInterface;
-use Knp\Component\Pager\PaginatorInterface;
-
 /**
  * @extends ServiceEntityRepository<Author>
  */
 class AuthorRepository extends ServiceEntityRepository
 {
-    private PaginatorInterface $paginator;
 
-    public function __construct(ManagerRegistry $registry, PaginatorInterface $paginator)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Author::class);
 
-        $this->paginator = $paginator;
-    }
-
-    public function findPaginatedAuthorList(int $page, int $limit): PaginationInterface
-    {
-        $qb = $this->createQueryBuilder('a');
-
-        return $this->paginator->paginate(
-            $qb->getQuery(),
-            $page,
-            $limit
-        );
     }
 }
